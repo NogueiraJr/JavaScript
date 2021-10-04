62d7d8177f3d045a9a987d3a13e95bda5c35e70a
Nogueira Jr <nogueirajr@hotmail.com>
2021-10-04 15:44:31 -0300

    Create p_004.js

---
+++ p_004.js
@@ -0,0 +1,15 @@
+//Area When Base and Height is Known
+
+const baseValue = prompt('Enter the base of a triangle: ');
+const heightValue = prompt('Enter the height of a triangle: ');
+
+// calculate the area
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
+
+//Enter the base of a triangle: 4
+//Enter the height of a triangle: 6
+//The area of the triangle is 12
