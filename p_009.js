497b2e6a0e48060d7f3a18d4b350eb70c89f9153
Nogueira Jr <nogueirajr@hotmail.com>
2021-10-11 18:55:49 -0300

    Create p_009.js

---
+++ p_009.js
@@ -0,0 +1,15 @@
+//Generate a Random Number
+
+const a = Math.random();
+console.log(a);
+
+//0.5856407221615856
+
+//-----------------
+
+//Get a Random Number between 1 and 10
+
+const a = Math.random() * (10-1) + 1
+console.log(`Random value between 1 and 10 is ${a}`);
+
+//Random value between 1 and 10 is 7.392579122270686
