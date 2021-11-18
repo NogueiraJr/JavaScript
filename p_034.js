43adf78f5fec4e1237c687d5c83e849b82b11139
Nogueira Jr <nogueirajr@hotmail.com>
2021-11-18 07:49:16 -0300

    Create p_034.js

---
+++ p_034.js
@@ -0,0 +1,24 @@
+// program to sort words in alphabetical order
+
+// take input
+const string = prompt('Enter a sentence: ');
+
+// converting to an array
+const words = string.split(' ');
+
+// sort the array elements
+words.sort();
+
+// display the sorted words
+console.log('The sorted words are:');
+
+for (const element of words) {
+  console.log(element);
+}
+
+// Enter a sentence: I am learning JavaScript
+// The sorted words are:
+// I
+// JavaScript
+// am
+// learning
