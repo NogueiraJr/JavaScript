d6fc3ca7868463b8bbe78db2a1f9be37a644c3d0
Nogueira Jr <nogueirajr@hotmail.com>
2021-11-29 09:32:05 -0300

    Create p_39.js

---
+++ p_39.js
@@ -0,0 +1,18 @@
+// program to convert first letter of a string to uppercase
+function capitalizeFirstLetter(str) {
+
+    // converting first letter to uppercase
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return capitalized;
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+const result = capitalizeFirstLetter(string);
+
+console.log(result);
+
+//Enter a string: javaScript
+//JavaScript
