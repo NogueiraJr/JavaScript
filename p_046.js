25dab06b99e2bbdb0781b267e12f9635864e96b8
Nogueira Jr <nogueirajr@hotmail.com>
2021-12-08 17:13:16 -0300

    Create p_046.js

---
+++ p_046.js
@@ -0,0 +1,23 @@
+// program to merge property of two objects
+
+// object 1
+const person = {
+    name: 'Jack',
+    age:26
+}
+
+// object 2
+const student = {
+    gender: 'male'
+}
+
+// merge two objects
+const newObj = Object.assign(person, student);
+
+console.log(newObj);
+
+// {
+//     name: "Jack",
+//     age: 26,
+//     gender: "male"
+// }
