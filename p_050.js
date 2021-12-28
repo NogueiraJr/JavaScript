105d5ffaa08c8e0842daa50eaec8c00671314a82
Nogueira Jr <nogueirajr@hotmail.com>
2021-12-27 22:25:59 -0300

    Create p_050.js

---
+++ p_050.js
@@ -0,0 +1,12 @@
+// program to create a multiline strings
+
+// using the + operator
+const message = 'This is a long message\n' + 
+    'that spans across multiple lines\n' + 
+    'in the code.'
+
+console.log(message);
+
+//This is a long message
+//that spans across multiple lines
+//in the code.
