b39e13dea3c7eef17c90c6c6bfe54be6da4e10a4
Nogueira Jr <nogueirajr@hotmail.com>
2022-01-03 19:45:10 -0300

    Create p_051.js

---
+++ p_051.js
@@ -0,0 +1,9 @@
+// program to format numbers as currency string
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+formatter.format(2500);
+
+//$2,500.00
