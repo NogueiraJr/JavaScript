ecf35ceb2189dec9bb4879dc3e2250d66fcc6f3d
Nogueira Jr <nogueirajr@hotmail.com>
2022-01-05 20:31:59 -0300

    Create p_053.js

---
+++ p_053.js
@@ -0,0 +1,14 @@
+// program to check if a string starts with another string
+
+const string = 'hello world';
+
+const toCheckString = 'he';
+
+if(string.startsWith(toCheckString)) {
+    console.warn('The string starts with "he".');
+}
+else {
+    console.warn(`The string does not starts with "he".`);
+}
+
+//The string starts with "he".
