886f017c2e2943d26b7ec3f243db1888717325af
Nogueira Jr <nogueirajr@hotmail.com>
2022-01-10 19:22:21 -0300

    Create p_055.js

---
+++ p_055.js
@@ -0,0 +1,14 @@
+// program to convert an object to a string
+
+const person = {
+    name: 'Jack',
+    age: 27
+}
+
+const result =  JSON.stringify(person);
+
+console.log(result);
+console.log(typeof result);
+
+//{"name":"Jack","age":27}
+//string
