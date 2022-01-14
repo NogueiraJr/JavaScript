292edc91131e1e68c4f255dca94a3938c1dc422b
Nogueira Jr <nogueirajr@hotmail.com>
2022-01-14 19:49:29 -0300

    Create p_058.js

---
+++ p_058.js
@@ -0,0 +1,14 @@
+// program to encode a string to Base64
+// defining the string
+const str = "Learning JavaScript"; 
+
+// encoding the string
+const result = window.btoa(str);
+console.log(result);
+
+// decoding the string
+const result1 = window.atob(result);
+console.log(result1);
+
+//TGVhcm5pbmcgSmF2YVNjcmlwdA==
+//Learning JavaScript
