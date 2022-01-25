86523a7a4e0adb15c821d2bfdf4274ab23399d33
Nogueira Jr <nogueirajr@hotmail.com>
2022-01-25 13:53:37 -0300

    Create p_062.js

---
+++ p_062.js
@@ -0,0 +1,19 @@
+// program to check leap year
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+// take input
+const year = prompt('Enter a year:');
+
+checkLeapYear(year);
+
+//Enter a year: 2000
+//2000 is a leap year
+
