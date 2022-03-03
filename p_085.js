50c11ba47f7976abc1ff7a8fb767a402bcb32b56
Nogueira Jr <nogueirajr@hotmail.com>
2022-03-03 07:44:04 -0300

    Create p_085.js

---
+++ p_085.js
@@ -0,0 +1,23 @@
+// program to check if a variable is undefined or null
+
+function checkVariable(variable) {
+
+    if(variable == null) {
+        console.log('The variable is undefined or null');
+    }
+    else {
+       console.log('The variable is neither undefined nor null');
+    }
+}
+
+let newVariable;
+
+checkVariable(5);
+checkVariable('hello');
+checkVariable(null);
+checkVariable(newVariable);
+
+//The variable is neither undefined nor null
+// The variable is neither undefined nor null
+// The variable is undefined or null
+// The variable is undefined or null
