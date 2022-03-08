3536268d94fe818ac64071311c085034d8e42efd
Nogueira Jr <nogueirajr@hotmail.com>
2022-03-08 08:25:08 -0300

    Create p_088.js

---
+++ p_088.js
@@ -0,0 +1,13 @@
+// input from the user
+const min = parseInt(prompt("Enter a min value: "));
+const max = parseInt(prompt("Enter a max value: "));
+
+// generating a random number
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
+
+//Enter a min value: 1
+//Enter a min value: 50
+//Random value between 1 and 50 is 47
