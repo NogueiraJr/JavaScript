10faacdd8c704a9aaa6bc98b909a84ac4cbc14f3
Nogueira Jr <nogueirajr@hotmail.com>
2022-03-09 05:42:18 -0300

    Create p_089.js

---
+++ p_089.js
@@ -0,0 +1,9 @@
+// program to get the URL
+
+const url1 = window.location.href;
+const url2 = document.URL;
+console.log(url1);
+console.log(url2);
+
+//https://www.google.com/
+//https://www.google.com/
