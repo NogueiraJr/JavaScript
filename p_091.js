baca76d312426ac069b637e7e41adac6efbb64ab
Nogueira Jr <nogueirajr@hotmail.com>
2022-03-11 08:06:00 -0300

    Update p_091.js

--- p_091.js
+++ p_091.js
@@ -47,3 +47,30 @@ testVariable(x);
 
 // The variable is not of function type
 // The variable is of function type
+
+// ---
+
+// Example 3: Using Object.prototype.toString.call() Method
+
+// program to check if a variable is of function type
+
+function testVariable(variable) {
+      
+    if(Object.prototype.toString.call(variable) == '[object Function]') {
+        console.log('The variable is of function type');
+    }
+    else {
+        console.log('The variable is not of function type');
+    }
+}
+
+const count = true;
+const x = function() {
+    console.log('hello')
+};
+
+testVariable(count);
+testVariable(x);
+
+// The variable is not of function type
+// The variable is of function type
